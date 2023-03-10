cab8c03766d55a669809fabd276c4636d3d80410
LuisEduNasc <luis.nacimento@cravehq.com>
2023-03-10 17:37:57 -0300

    feat(schema): add user schema

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql').graphqlHTTP;
+const schema = require('./schema/schema');
 
 const app = express();
 const PORT = 4000;
 
 app.use('/graphql', expressGraphQL({
+  schema,
   graphiql: true
 }));
 
